a761427365323298f09b5c605c862bae76b09a0a
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-11-03 12:36:50 -0300

    implementação de consultas básicas de musica

--- src/main/java/com/cirofreitas/API/Musica/controller/MusicaController.java
+++ src/main/java/com/cirofreitas/API/Musica/controller/MusicaController.java
@@ -2,6 +2,7 @@ package com.cirofreitas.API.Musica.controller;
 
 import com.cirofreitas.API.Musica.dto.MusicaDto;
 import com.cirofreitas.API.Musica.model.Musica;
+import com.cirofreitas.API.Musica.model.Origem;
 import com.cirofreitas.API.Musica.repository.MusicaRepository;
 import com.cirofreitas.API.Musica.service.MusicaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/musica")
+@RequestMapping(path = "/musicas")
 public class MusicaController {
     @Autowired
     private MusicaRepository repository;
@@ -22,6 +23,14 @@ public class MusicaController {
     public List<Musica> findAll() {
         return repository.findAll();
     }
+
+    @GetMapping("{id}")
+    public Musica findById(@PathVariable("id") Integer id) {
+        return service.findById(id);
+    }
+
+    @GetMapping("{id}/origens")
+    public List<Origem> listarOrigens(@PathVariable("id") Integer id) { return service.listarOrigens(id); }
 
     @PostMapping
     public void save(@RequestBody MusicaDto musica) { service.save(musica); }
--- src/main/java/com/cirofreitas/API/Musica/service/MusicaService.java
+++ src/main/java/com/cirofreitas/API/Musica/service/MusicaService.java
@@ -1,17 +1,31 @@
 package com.cirofreitas.API.Musica.service;
 
 import com.cirofreitas.API.Musica.dto.MusicaDto;
+import com.cirofreitas.API.Musica.infra.MissingEntityException;
 import com.cirofreitas.API.Musica.model.Musica;
+import com.cirofreitas.API.Musica.model.Origem;
 import com.cirofreitas.API.Musica.repository.MusicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class MusicaService {
     @Autowired
     private MusicaRepository repository;
+
+    public Musica findById(Integer id) {
+        Optional<Musica> musica = repository.findById(id);
+
+        if(musica.isPresent())
+            throw new MissingEntityException("Não foi encontrado nenhum album com este id!");
+
+        return musica.get();
+    }
+
+    public List<Origem> listarOrigens(Integer id) { return this.findById(id).getOrigens(); }
 
     public void save(MusicaDto musica) {
         Optional<Musica> musicaPresente = repository.findMusicaByOrigem(musica.getIdOrigem());
