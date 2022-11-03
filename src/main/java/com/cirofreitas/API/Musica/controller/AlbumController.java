89a418f3d8df9684e4da3500253373228f800e00
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-11-03 12:25:52 -0300

    implementação de consultas básicas de albuns

--- src/main/java/com/cirofreitas/API/Musica/controller/AlbumController.java
+++ src/main/java/com/cirofreitas/API/Musica/controller/AlbumController.java
@@ -2,6 +2,8 @@ package com.cirofreitas.API.Musica.controller;
 
 import com.cirofreitas.API.Musica.dto.AlbumDto;
 import com.cirofreitas.API.Musica.model.Album;
+import com.cirofreitas.API.Musica.model.Musica;
+import com.cirofreitas.API.Musica.model.Origem;
 import com.cirofreitas.API.Musica.repository.AlbumRepository;
 import com.cirofreitas.API.Musica.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/album")
+@RequestMapping(path = "/albuns")
 public class AlbumController {
     @Autowired
     private AlbumRepository repository;
@@ -22,6 +24,17 @@ public class AlbumController {
     public List<Album> findAll() {
         return repository.findAll();
     }
+
+    @GetMapping("{id}")
+    public Album findById(@PathVariable("id") Integer id) {
+        return service.findById(id);
+    }
+
+    @GetMapping("{id}/musicas")
+    public List<Musica> listarMusicas(@PathVariable("id") Integer id) { return service.listarMusicas(id); }
+
+    @GetMapping("{id}/origens")
+    public List<Origem> listarOrigens(@PathVariable("id") Integer id) { return service.listarOrigens(id); }
 
     @PostMapping
     public void save(@RequestBody AlbumDto album) { service.save(album); }
--- src/main/java/com/cirofreitas/API/Musica/service/AlbumService.java
+++ src/main/java/com/cirofreitas/API/Musica/service/AlbumService.java
@@ -1,18 +1,35 @@
 package com.cirofreitas.API.Musica.service;
 
 import com.cirofreitas.API.Musica.dto.AlbumDto;
+import com.cirofreitas.API.Musica.infra.MissingEntityException;
 import com.cirofreitas.API.Musica.model.Album;
 import com.cirofreitas.API.Musica.model.Musica;
+import com.cirofreitas.API.Musica.model.Origem;
 import com.cirofreitas.API.Musica.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AlbumService {
     @Autowired
     private AlbumRepository repository;
+
+    public Album findById(Integer id) {
+        Optional<Album> album = repository.findById(id);
+
+        if(album.isPresent())
+            throw new MissingEntityException("Não foi encontrado nenhum album com este id!");
+
+        return album.get();
+    }
+
+    public List<Origem> listarOrigens(Integer id) { return this.findById(id).getOrigens(); }
+
+    public List<Musica> listarMusicas(Integer id) { return this.findById(id).getMusicas(); }
 
     public void save(AlbumDto album) {
         Album novoAlbum = album.dtoToModel();
